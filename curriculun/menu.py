bccc77e57e0c09acbfa04130221f7d4edca7f0c8
code-bots-dev <codebotsdev@gmail.com>
2025-10-06 17:10:04 -0300

    Add files via upload

---
+++ curriculun/menu.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import curriculo, projetos
+
+def menu_inicial():
+    pg = st.navigation([
+        st.Page("curriculo.py", title="Curriculo Vitor Rafael", icon="📝"),
+        st.Page("projetos.py", title="Projetos", icon="✅")
+    ],
+        position="sidebar",
+        expanded=True
+    )
+
+menu_inicial()
